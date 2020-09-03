a2066231adb8fd39295aa2b528102642c8bc1252
iamsomraj <iamsomraj@gmail.com>
2020-09-03 23:04:24 +0530

    cust based on cust city updated

--- Sprint 3/Java Customers Based on Customer City/Source.java
+++ Sprint 3/Java Customers Based on Customer City/Source.java
@@ -1,8 +1,8 @@
 import java.util.Scanner;
 
 public class Source {
-   String[][] toys = new String[5][5];
-   String[][] customers = new String[5][5];
+   static String[][] toys = new String[5][5];
+   static String[][] customers = new String[5][3];
 
    Source() {
       toys[0][0] = "1";
@@ -20,38 +20,6 @@ public class Source {
       toys[4][0] = "5";
       toys[4][1] = "Chutes and Ladders";
 
-      // Cant expect more than this
-      // they have not even provided the details of customer -_-
-      // that's why, I am making dummy data
-
-      int i = 0;
-      customers[i][0] = "sample-id-" + i;
-      customers[i][1] = "sample-name-" + i;
-      customers[i][2] = "sample-address-" + i;
-      i++;
-      customers[i][0] = "sample-id-" + i;
-      customers[i][1] = "sample-name-" + i;
-      customers[i][2] = "sample-address-" + i;
-      i++;
-      customers[i][0] = "sample-id-" + i;
-      customers[i][1] = "sample-name-" + i;
-      customers[i][2] = "sample-address-" + i;
-      i++;
-      customers[i][0] = "sample-id-" + i;
-      customers[i][1] = "sample-name-" + i;
-      customers[i][2] = "sample-address-" + i;
-
-      // only proper data that I could use
-      // source : test case
-
-      i++;
-      customers[i][0] = "205";
-      customers[i][1] = "Kiran";
-      customers[i][2] = "Mumbai";
-
-      // would not do the next problems
-      // Update : 3rd September,2020
-
    }
 
    public String getToy(int age) {
@@ -61,21 +29,49 @@ public class Source {
          }
       }
       return "no toys available";
+
    }
 
-   public String[] getCustomer(String name) {
-      for (String[] customer : customers) {
-         if (customer[1].equals(name)) {
-            return customer;
+   public static String[] getCustomer(String name) {
+      customers[0][0] = "102";
+      customers[0][1] = "Krithick";
+      customers[0][2] = "Bangalore";
+
+      customers[1][0] = "105";
+      customers[1][1] = "Prabu";
+      customers[1][2] = "Chennai";
+
+      customers[2][0] = "252";
+      customers[2][1] = "Chandrav";
+      customers[2][2] = "Delhi";
+
+      customers[3][0] = "205";
+      customers[3][1] = "Kiran";
+      customers[3][2] = "Mumbai";
+
+      customers[4][0] = "202";
+      customers[4][1] = "Shravan";
+      customers[4][2] = "Pune";
+
+      int low = 0;
+      int high = customers.length - 1;
+      while (low <= high) {
+         int mid = (low + high) / 2;
+         if (customers[mid][2].compareTo(name) == 0) {
+            return customers[mid];
+         } else if (customers[mid][2].compareTo(name) >= 0) {
+            high = mid - 1;
+         } else {
+            low = mid + 1;
          }
       }
-      return new String[] { "no-id-found", "no-name-found", "no-address-found" };
+      return null;
    }
 
    public static void main(String[] args) {
       Scanner scanner = new Scanner(System.in);
       String customerName = scanner.nextLine();
-      String customer[] = new Source().getCustomer(customerName);
+      String customer[] = getCustomer(customerName);
       System.out.println(customer[0] + " " + customer[1] + " " + customer[2]);
    }
 }
