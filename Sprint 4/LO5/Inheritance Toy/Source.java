0842c442ebbe4a6cd9358e84652abec2aa3e4914
iamsomraj <iamsomraj@gmail.com>
2020-09-15 15:17:57 +0530

    Inheritance Toy done 100%

--- Sprint 4/LO5/Inheritance Toy/Source.java
+++ Sprint 4/LO5/Inheritance Toy/Source.java
@@ -1,6 +1,10 @@
-// no score
-
 import java.util.*;
+
+interface AdminService {
+}
+
+class AdminServiceImpl implements AdminService {
+}
 
 class Toy {
    private int toyId;
@@ -8,10 +12,23 @@ class Toy {
    private String toyType;
    private int minAge;
    private int maxAge;
-   private float price;
+   private double price;
    private int quantity;
-   private float rentalAmount;
-   private float refundableDeposit;
+   private double rentalAmount;
+   private double refundableDeposit;
+
+   Toy(int toyId, String toyName, String toyType, int minAge, int maxAge, double price, int quantity,
+         double rentalAmount, double refundableDeposit) {
+      this.toyId = toyId;
+      this.toyName = toyName;
+      this.toyType = toyType;
+      this.minAge = minAge;
+      this.maxAge = maxAge;
+      this.price = price;
+      this.quantity = quantity;
+      this.rentalAmount = rentalAmount;
+      this.refundableDeposit = refundableDeposit;
+   }
 
    public int getToyId() {
       return toyId;
@@ -25,336 +42,101 @@ class Toy {
       return toyName;
    }
 
-   public void setToyName(String toyName) {
-      this.toyName = toyName;
-   }
-
    public String getToyType() {
       return toyType;
-   }
-
-   public void setToyType(String toyType) {
-      this.toyType = toyType;
    }
 
    public int getMinAge() {
       return minAge;
    }
 
-   public void setMinAge(int minAge) {
-      this.minAge = minAge;
-   }
-
    public int getMaxAge() {
       return maxAge;
    }
 
-   public void setMaxAge(int maxAge) {
-      this.maxAge = maxAge;
-   }
-
-   public float getPrice() {
+   public double getPrice() {
       return price;
-   }
-
-   public void setPrice(float price) {
-      this.price = price;
    }
 
    public int getQuantity() {
       return quantity;
    }
 
-   public void setQuantity(int quantity) {
-      this.quantity = quantity;
-   }
-
-   public float getRentalAmount() {
+   public double getRentalAmount() {
       return rentalAmount;
    }
 
-   public void setRentalAmount(float rentalAmount) {
-      this.rentalAmount = rentalAmount;
-   }
-
-   public float getRefundableDeposit() {
+   public double getRefundableDeposit() {
       return refundableDeposit;
    }
 
-   public void setRefundableDeposit(float refundableDeposit) {
-      this.refundableDeposit = refundableDeposit;
-   }
-
-   public Toy(int toyId, String toyName, String toyType, int minAge, int maxAge, float price, int quantity,
-         float rentalAmount, float refundableDeposit) {
-      this.toyId = toyId;
+   public void setToyName(String toyName) {
       this.toyName = toyName;
+   }
+
+   public void setToyType(String toyType) {
       this.toyType = toyType;
+
+   }
+
+   public void setMinAge(int minAge) {
       this.minAge = minAge;
+
+   }
+
+   public void setMaxAge(int maxAge) {
       this.maxAge = maxAge;
+
+   }
+
+   public void setPrice(double price) {
       this.price = price;
+
+   }
+
+   public void setQuantity(int quantity) {
       this.quantity = quantity;
+
+   }
+
+   public void setRentalAmount(double rentalAmount) {
       this.rentalAmount = rentalAmount;
+
+   }
+
+   public void setRefundableDeposit(double refundableDeposit) {
       this.refundableDeposit = refundableDeposit;
    }
-
-   @Override
-   public String toString() {
-      String result = "";
-      result += "ToyName: " + toyName + "\n";
-      result += "ToyType: " + toyType + "\n";
-      result += "Quantity: " + quantity + "\n";
-      result += "RentalAmount: " + rentalAmount;
-      return result;
-   }
-
 }
 
 class ElectronicToy extends Toy {
-   private int numberOfBatteries;
-   private String operatingMode;
+   public int numberOfBatteries;
+   public String operatingMode;
 
-   public ElectronicToy(int toyId, String toyName, String toyType, int minAge, int maxAge, float price, int quantity,
-         float rentalAmount, float refundableDeposit, int numberOfBatteries, String operatingMode) {
+   ElectronicToy(int toyId, String toyName, String toyType, int minAge, int maxAge, double price, int quantity,
+         double rentalAmount, double refundableDeposit, int numberOfBatteries, String operatingMode) {
       super(toyId, toyName, toyType, minAge, maxAge, price, quantity, rentalAmount, refundableDeposit);
       this.numberOfBatteries = numberOfBatteries;
       this.operatingMode = operatingMode;
    }
 
-   public int getNumberOfBatteries() {
-      return numberOfBatteries;
-   }
-
-   public void setNumberOfBatteries(int numberOfBatteries) {
-      this.numberOfBatteries = numberOfBatteries;
-   }
-
-   public String getOperatingMode() {
-      return operatingMode;
-   }
-
-   public void setOperatingMode(String operatingMode) {
-      this.operatingMode = operatingMode;
-   }
-
-   @Override
    public String toString() {
-      String result = "";
-      result += "Name: " + this.getToyName() + "\n";
-      result += "Type: " + this.getToyType() + "\n";
-      result += "Mode: " + this.getOperatingMode() + "\n";
-      result += "Batteries: " + this.getNumberOfBatteries();
-      return result;
+      return "ToyName: " + super.getToyName() + "\nType: " + super.getToyType() + "\nMode: " + operatingMode
+            + "\nBatteries: " + numberOfBatteries;
    }
-
 }
 
 class MusicalToy extends Toy {
-   private int noOfSpeakers;
+   public int noOfSpeakers;
 
-   public MusicalToy(int toyId, String toyName, String toyType, int minAge, int maxAge, float price, int quantity,
-         float rentalAmount, float refundableDeposit, int noOfSpeakers) {
+   MusicalToy(int toyId, String toyName, String toyType, int minAge, int maxAge, double price, int quantity,
+         double rentalAmount, double refundableDeposit, int noOfSpeakers) {
       super(toyId, toyName, toyType, minAge, maxAge, price, quantity, rentalAmount, refundableDeposit);
       this.noOfSpeakers = noOfSpeakers;
    }
 
-   public int getNoOfSpeakers() {
-      return noOfSpeakers;
-   }
-
-   public void setNoOfSpeakers(int noOfSpeakers) {
-      this.noOfSpeakers = noOfSpeakers;
-   }
-
-   @Override
    public String toString() {
-      String result = "";
-      result += "Name: " + this.getToyName() + "\n";
-      result += "Type: " + this.getToyType() + "\n";
-      result += "Speakers: " + this.getNoOfSpeakers();
-      return result;
-   }
-
-}
-
-interface AdminService {
-   boolean validateAdmin(String email, String password);
-}
-
-interface CustomerService {
-   boolean validateCustomer(String email, String password);
-}
-
-class Customer {
-   private int customerId;
-   private String customerName;
-   private String email;
-   private String password;
-   private String address;
-
-   public Customer(int customerId, String customerName, String email, String password, String address) {
-      this.customerId = customerId;
-      this.customerName = customerName;
-      this.password = password;
-      this.address = address;
-      this.email = email;
-   }
-
-   public String getPassword() {
-      return password;
-   }
-
-   public String getEmail() {
-      return email;
-   }
-
-   public int getCustomerId() {
-      return customerId;
-   }
-
-   public void setCustomerId(int customerId) {
-      this.customerId = customerId;
-   }
-
-   public String getCustomerName() {
-      return customerName;
-   }
-
-   public void setCustomerName(String customerName) {
-      this.customerName = customerName;
-   }
-
-   public void setEmail(String email) {
-      this.email = email;
-   }
-
-   public void setPassword(String password) {
-      this.password = password;
-   }
-
-   public String getAddress() {
-      return address;
-   }
-
-   public void setAddress(String address) {
-      this.address = address;
-   }
-
-   @Override
-   public String toString() {
-      return "Customer [address=" + address + ", customerId=" + customerId + ", customerName=" + customerName
-            + ", email=" + email + ", password=" + password + "]";
-   }
-
-}
-
-class Admin {
-   private String name;
-   private String email;
-   private String password;
-
-   public Admin(String name, String email, String password) {
-      this.password = password;
-      this.name = name;
-      this.email = email;
-   }
-
-   public String getPassword() {
-      return password;
-   }
-
-   public String getEmail() {
-      return email;
-   }
-
-   public String getName() {
-      return name;
-   }
-
-   public void setName(String name) {
-      this.name = name;
-   }
-
-   public void setEmail(String email) {
-      this.email = email;
-   }
-
-   public void setPassword(String password) {
-      this.password = password;
-   }
-
-   @Override
-   public String toString() {
-      return "Admin [email=" + email + ", name=" + name + ", password=" + password + "]";
-   }
-
-}
-
-class AdminServiceImpl extends CustomerServiceImpl implements AdminService {
-   static Toy availableToys[] = new Toy[4];
-
-   public static Admin[] adminArray = new Admin[5];
-
-   AdminServiceImpl() {
-      adminArray[0] = new Admin("Krithick", "krithick@gmail.com", "krithi");
-      adminArray[1] = new Admin("Rajan", "rajan@gmail.com", "rajan#345");
-      adminArray[2] = new Admin("Chandrav", "chand@gmail.com", "wel$234");
-      adminArray[3] = new Admin("Ankit", "ankit@gmail.com", "kit@56");
-      adminArray[4] = new Admin("Akilan", "akilan@gmail.com", "ak*76");
-      availableToys[0] = new Toy(120, "Rubber Ducky", "Toy", 1, 3, 200, 20, 20, 200);
-      availableToys[1] = new Toy(130, "Car", "Toy", 1, 5, 100, 30, 20, 100);
-      availableToys[2] = new Toy(150, "Kite", "Toy", 3, 8, 100, 50, 20, 50);
-      availableToys[3] = new Toy(180, "Airplane", "Toy", 4, 7, 500, 30, 50, 20);
-   }
-
-   public boolean validateAdmin(String email, String password) {
-      for (Admin a : adminArray) {
-         if (a.getEmail().equals(email) && a.getPassword().equals(password)) {
-            return true;
-         }
-      }
-      return false;
-   }
-
-   public Toy searchToy(int toyId) {
-      for (Toy toy : availableToys) {
-         if (toy.getToyId() == toyId) {
-            return toy;
-         }
-      }
-      return null;
-   }
-
-}
-
-class CustomerServiceImpl implements CustomerService {
-   public static Customer[] customerArray = new Customer[5];
-
-   public CustomerServiceImpl() {
-      customerArray[0] = new Customer(100, "Karthi", "kar@gmail.com", "kar#2", "Bangalore");
-      customerArray[1] = new Customer(101, "Kumar", "km@mail.com", "km#2", "Bangalore");
-      customerArray[2] = new Customer(102, "Rakesh", "rak@mail.com", "rak#45", "Chennai");
-      customerArray[3] = new Customer(103, "Rakshan", "shan@mail.com", "an#2", "Mumbai");
-      customerArray[4] = new Customer(104, "Virat", "rat@mail.com", "at#45", "Pune");
-
-   }
-
-   public Customer search(int CustomerId) {
-      for (Customer Customer : customerArray) {
-         if (Customer.getCustomerId() == CustomerId) {
-            return Customer;
-         }
-      }
-      return null;
-   }
-
-   public boolean validateCustomer(String email, String password) {
-      for (Customer c : customerArray) {
-         if (c.getEmail().equals(email) && c.getPassword().equals(password)) {
-            return true;
-         }
-      }
-      return false;
+      return "ToyName: " + super.getToyName() + "\nType: Musical " + super.getToyType() + "\nSpeaker: " + noOfSpeakers;
    }
 }
 
@@ -376,45 +158,44 @@ public class Source {
 
       toys[4][0] = "5";
       toys[4][1] = "Chutes and Ladders";
-
    }
 
+   /*
+    * public String getToy(int age) { }
+    */
+
    public static void main(String[] args) {
+      AdminService adminService = new AdminServiceImpl();
       Scanner scanner = new Scanner(System.in);
       int n = Integer.parseInt(scanner.nextLine());
+
       if (n == 1) {
-         int toyId = Integer.parseInt(scanner.nextLine());
          String toyName = scanner.nextLine();
          String toyType = scanner.nextLine();
          int minAge = Integer.parseInt(scanner.nextLine());
          int maxAge = Integer.parseInt(scanner.nextLine());
-         float price = Float.parseFloat(scanner.nextLine());
+         double price = Double.parseDouble(scanner.nextLine());
          int quantity = Integer.parseInt(scanner.nextLine());
-         float rentalAmount = Float.parseFloat(scanner.nextLine());
-         float refundableDeposit = Float.parseFloat(scanner.nextLine());
+         double rentalAmount = Double.parseDouble(scanner.nextLine());
+         double refundableDeposit = Double.parseDouble(scanner.nextLine());
          int numberOfBatteries = Integer.parseInt(scanner.nextLine());
          String operatingMode = scanner.nextLine();
-         ElectronicToy electronicToy = new ElectronicToy(toyId, toyName, toyType, minAge, maxAge, price, quantity,
-               rentalAmount, refundableDeposit, numberOfBatteries, operatingMode);
-
-         System.out.println(electronicToy);
-
+         ElectronicToy e = new ElectronicToy(1, toyName, toyType, minAge, maxAge, price, quantity, rentalAmount,
+               refundableDeposit, numberOfBatteries, operatingMode);
+         System.out.println(e.toString());
       } else if (n == 2) {
-         int toyId = Integer.parseInt(scanner.nextLine());
          String toyName = scanner.nextLine();
          String toyType = scanner.nextLine();
          int minAge = Integer.parseInt(scanner.nextLine());
          int maxAge = Integer.parseInt(scanner.nextLine());
-         float price = Float.parseFloat(scanner.nextLine());
+         double price = Double.parseDouble(scanner.nextLine());
          int quantity = Integer.parseInt(scanner.nextLine());
-         float rentalAmount = Float.parseFloat(scanner.nextLine());
-         float refundableDeposit = Float.parseFloat(scanner.nextLine());
+         double rentalAmount = Double.parseDouble(scanner.nextLine());
+         double refundableDeposit = Double.parseDouble(scanner.nextLine());
          int noOfSpeakers = Integer.parseInt(scanner.nextLine());
-         MusicalToy musicalToy = new MusicalToy(toyId, toyName, toyType, minAge, maxAge, price, quantity, rentalAmount,
+         MusicalToy m = new MusicalToy(1, toyName, toyType, minAge, maxAge, price, quantity, rentalAmount,
                refundableDeposit, noOfSpeakers);
-         System.out.println(musicalToy);
-
+         System.out.println(m.toString());
       }
-
    }
 }
