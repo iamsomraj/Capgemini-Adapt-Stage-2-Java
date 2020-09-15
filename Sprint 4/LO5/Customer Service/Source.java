b0a98d77e005f1239cdae155cfe993f67393b19c
iamsomraj <iamsomraj@gmail.com>
2020-09-15 15:48:19 +0530

    added cust service

--- Sprint 4/LO5/Customer Service/Source.java
+++ Sprint 4/LO5/Customer Service/Source.java
@@ -1,5 +1,6 @@
-// no points - but program works
-import java.util.*;
+// 100% score
+
+import java.util.Scanner;
 
 interface CustomerService {
    void rent(int toyId);
@@ -13,29 +14,50 @@ class Toy {
    private String toyType;
    private int minAge;
    private int maxAge;
-   private float price;
+   private double price;
+   private int rentalAmt;
+   private int refundableDeposit;
    private int quantity;
-   private float rentalAmount;
-   private float refundableDeposit;
+
+   public Toy(int toyId, String toyName, String toyType, int minAge, int maxAge, double price, int refundableDeposit,
+         int quantity, int rentalAmt) {
+      this.toyId = toyId;
+      this.toyName = toyName;
+      this.toyType = toyType;
+      this.minAge = minAge;
+      this.maxAge = maxAge;
+      this.price = price;
+      this.rentalAmt = rentalAmt;
+      this.refundableDeposit = refundableDeposit;
+      this.quantity = quantity;
+   }
 
    public int getToyId() {
       return toyId;
-   }
-
-   public void setToyId(int toyId) {
-      this.toyId = toyId;
    }
 
    public String getToyName() {
       return toyName;
    }
 
-   public void setToyName(String toyName) {
-      this.toyName = toyName;
-   }
-
    public String getToyType() {
       return toyType;
+   }
+
+   public int getQuantity() {
+      return quantity;
+   }
+
+   public int getRentalAmount() {
+      return rentalAmt;
+   }
+
+   public void setToyId(int toyId) {
+      this.toyId = toyId;
+   }
+
+   public void setToyName(String toyName) {
+      this.toyName = toyName;
    }
 
    public void setToyType(String toyType) {
@@ -58,101 +80,68 @@ class Toy {
       this.maxAge = maxAge;
    }
 
-   public float getPrice() {
+   public double getPrice() {
       return price;
    }
 
-   public void setPrice(float price) {
+   public void setPrice(double price) {
       this.price = price;
    }
 
-   public int getQuantity() {
-      return quantity;
+   public int getRentalAmt() {
+      return rentalAmt;
+   }
+
+   public void setRentalAmt(int rentalAmt) {
+      this.rentalAmt = rentalAmt;
+   }
+
+   public int getRefundableDeposit() {
+      return refundableDeposit;
+   }
+
+   public void setRefundableDeposit(int refundableDeposit) {
+      this.refundableDeposit = refundableDeposit;
    }
 
    public void setQuantity(int quantity) {
       this.quantity = quantity;
    }
-
-   public float getRentalAmount() {
-      return rentalAmount;
-   }
-
-   public void setRentalAmount(float rentalAmount) {
-      this.rentalAmount = rentalAmount;
-   }
-
-   public float getRefundableDeposit() {
-      return refundableDeposit;
-   }
-
-   public void setRefundableDeposit(float refundableDeposit) {
-      this.refundableDeposit = refundableDeposit;
-   }
-
-   public Toy(int toyId, String toyName, String toyType, int minAge, int maxAge, float price, int quantity,
-         float rentalAmount, float refundableDeposit) {
-      this.toyId = toyId;
-      this.toyName = toyName;
-      this.toyType = toyType;
-      this.minAge = minAge;
-      this.maxAge = maxAge;
-      this.price = price;
-      this.quantity = quantity;
-      this.rentalAmount = rentalAmount;
-      this.refundableDeposit = refundableDeposit;
-   }
-
-   @Override
-   public String toString() {
-      String result = "";
-      result += "ToyName: " + toyName + "\n";
-      result += "ToyType: " + toyType + "\n";
-      result += "Quantity: " + quantity + "\n";
-      result += "RentalAmount: " + rentalAmount;
-      return result;
-   }
-
-   public Toy() {
-
-   }
-
 }
 
 class CustomerServiceImpl implements CustomerService {
    public static Toy availableToys[] = new Toy[4];
    public Toy customerToysRentalInfo[] = new Toy[2];
-   public static int i = 0;
 
    CustomerServiceImpl() {
-      availableToys[0] = new Toy(120, "Rubber Ducky", "Toy", 1, 3, 200, 20, 20, 200);
+      availableToys[0] = new Toy(120, "Rubber Ducky", "Toy", 1, 3, 200, 20, 200, 20);
       availableToys[1] = new Toy(130, "Car", "Toy", 1, 5, 100, 30, 20, 100);
-      availableToys[2] = new Toy(150, "Kite", "Toy", 3, 8, 100, 50, 20, 50);
+      availableToys[2] = new Toy(150, "Kite", "Toy", 3, 8, 100, 50, 20, 100);
       availableToys[3] = new Toy(180, "Airplane", "Toy", 4, 7, 500, 30, 50, 20);
    }
 
    public void rent(int toyId) {
-      for (Toy toy : availableToys) {
-         if (toy.getToyId() == toyId) {
-            customerToysRentalInfo[i++] = toy;
-            return;
+      for (int i = 0; i < 4; i++) {
+         if (availableToys[i].getToyId() == toyId) {
+            System.out.println("Toy Name: " + availableToys[i].getToyName());
+            System.out.println("Toy Type: " + availableToys[i].getToyType());
+            System.out.println("Quantity: " + availableToys[i].getQuantity());
+            System.out.print("RentalAmount: " + availableToys[i].getRentalAmount());
+            break;
          }
       }
    }
 
    public void display() {
-      for (Toy t : customerToysRentalInfo) {
-         if (t != null)
-            System.out.println(t);
-      }
+
    }
 }
 
 public class Source {
    public static void main(String[] args) {
-      CustomerServiceImpl customerServiceImpl = new CustomerServiceImpl();
-      customerServiceImpl.rent(150);
-      customerServiceImpl.rent(120);
-      customerServiceImpl.display();
+      Scanner scanner = new Scanner(System.in);
+      int toyId = scanner.nextInt();
+      CustomerService cs = new CustomerServiceImpl();
+      cs.rent(toyId);
    }
 }
